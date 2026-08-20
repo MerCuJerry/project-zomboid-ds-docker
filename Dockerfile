deca9860fd6adf98cc54f8947655857626b446d0
MerCuJerry <mercujerry@gmail.com>
2026-08-20 10:38:47 +0800

    Squash merge from branch "master"

--- Dockerfile
+++ Dockerfile
@@ -1,32 +1,28 @@
 FROM cm2network/steamcmd:root
 
-ENV CONFIG_XMX 16
-ENV SERVER_NAME ZomboidDocker
-ENV ADMIN_PASSWORD ZomboidDockerAdmin
-ENV STEAM_TEST_BRANCH ""
-ENV ADDITIONAL_ARGS ""
+ENV CONFIG_XMX=16
+ENV SERVER_NAME=ZomboidDocker
+ENV ADMIN_PASSWORD=ZomboidDockerAdmin
+ENV STEAM_TEST_BRANCH=""
+ENV ADDITIONAL_ARGS=""
+ENV SKIP_INSTALL=false
 
-ENV STEAMAPPID 380870
-ENV STEAMAPP project-zomboid
-ENV STEAMAPPDIR "${HOMEDIR}/${STEAMAPP}-dedicated"
+ENV STEAMAPPID=380870
+ENV STEAMAPP=project-zomboid
+ENV STEAMAPPDIR="${HOMEDIR}/${STEAMAPP}-dedicated"
+
+ENV PUID=1000
+ENV PGID=1000
 
 # ARG DEBIAN_FRONTEND=noninteractive
-
-RUN apt-get update && apt upgrade -y && \
-    mkdir -p /usr/share/man/man1 && \
-    apt-get install -y \
-        libsdl2-2.0-0 \
-        default-jre 
-
 COPY entrypoint.sh ${HOMEDIR}/entrypoint.sh
-COPY Server ${HOMEDIR}/Server
-RUN chmod +x "${HOMEDIR}/entrypoint.sh" && chown -R "${USER}:${USER}" "${HOMEDIR}"
+RUN chmod +x "${HOMEDIR}/entrypoint.sh"
 
-USER ${USER}
+COPY Server ${HOMEDIR}/Server
 WORKDIR ${HOMEDIR}
 VOLUME ${STEAMAPPDIR}
 
-CMD ["bash", "entrypoint.sh"]
+CMD [ "bash", "entrypoint.sh" ]
 
 EXPOSE 16261/udp
 EXPOSE 16262/udp
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,38 +1,53 @@
-#!/bin/bash
+#!/bin/sh
 # VARIABLES
 export LD_LIBRARY_PATH=${STEAMAPPDIR}/jre64:$LD_LIBRARY_PATH
 CONFIG_PATH=${STEAMAPPDIR}/ProjectZomboid64.json
 
-if ! test -d /home/steam/Zomboid/Server; then
-    mkdir -p /home/steam/Zomboid/Server
-    echo "COPY configuration from <${HOMEDIR}/Server/ZomboidDocker*> to </home/steam/Zomboid/Server/${SERVER_NAME}*>"
-    if ! test -f /home/steam/Zomboid/Server/${SERVER_NAME}.ini; then
-    cp ${HOMEDIR}/Server/ZomboidDocker.ini /home/steam/Zomboid/Server/${SERVER_NAME}.ini
+if [ ! -z "$PUID" ] && [ "$PUID" != "$(id -u steam)" ]; then
+    usermod -o -u "$PUID" steam
+fi
+if [ ! -z "$PGID" ] && [ "$PGID" != "$(id -g steam)" ]; then
+    groupmod -o -g "$PGID" steam
+fi
+
+chown -R steam:steam ${HOMEDIR}
+
+su - steam
+export HOME=${HOMEDIR}
+
+if ! test -d ${HOMEDIR}/Zomboid/Server; then
+    mkdir -p ${HOMEDIR}/Zomboid/Server
+    echo "COPY configuration from <${HOMEDIR}/Server/ZomboidDocker*> to <${HOMEDIR}/Zomboid/Server/${SERVER_NAME}*>"
+    if ! test -f ${HOMEDIR}/Zomboid/Server/${SERVER_NAME}.ini; then
+    cp ${HOMEDIR}/Server/ZomboidDocker.ini ${HOMEDIR}/Zomboid/Server/${SERVER_NAME}.ini
     fi
-    if ! test -f /home/steam/Zomboid/Server/${SERVER_NAME}_SandboxVars.lua; then
-    cp ${HOMEDIR}/Server/ZomboidDocker_SandboxVars.lua /home/steam/Zomboid/Server/${SERVER_NAME}_SandboxVars.lua
+    if ! test -f ${HOMEDIR}/Zomboid/Server/${SERVER_NAME}_SandboxVars.lua; then
+    cp ${HOMEDIR}/Server/ZomboidDocker_SandboxVars.lua ${HOMEDIR}/Zomboid/Server/${SERVER_NAME}_SandboxVars.lua
     fi
-    if ! test -f /home/steam/Zomboid/Server/${SERVER_NAME}_spawnpoints.lua; then
-    cp ${HOMEDIR}/Server/ZomboidDocker_spawnpoints.lua /home/steam/Zomboid/Server/${SERVER_NAME}_spawnpoints.lua
+    if ! test -f ${HOMEDIR}/Zomboid/Server/${SERVER_NAME}_spawnpoints.lua; then
+    cp ${HOMEDIR}/Server/ZomboidDocker_spawnpoints.lua ${HOMEDIR}/Zomboid/Server/${SERVER_NAME}_spawnpoints.lua
     fi
-    if ! test -f /home/steam/Zomboid/Server/${SERVER_NAME}_spawnregions.lua; then
-    cp ${HOMEDIR}/Server/ZomboidDocker_spawnregions.lua /home/steam/Zomboid/Server/${SERVER_NAME}_spawnregions.lua
+    if ! test -f ${HOMEDIR}/Zomboid/Server/${SERVER_NAME}_spawnregions.lua; then
+    cp ${HOMEDIR}/Server/ZomboidDocker_spawnregions.lua ${HOMEDIR}/Zomboid/Server/${SERVER_NAME}_spawnregions.lua
     fi
 fi
 
 
 # Steamcmd install
-echo "APP <ID:${STEAMAPPID}> Install directory will be ${STEAMAPPDIR}"
-if ${STEAM_TEST_BRANCH} != ""; then
-    echo "Using steamcmd beta branch ${STEAM_TEST_BRANCH}"
-    ${STEAMCMDDIR}/steamcmd.sh +force_install_dir ${STEAMAPPDIR} +login anonymous +app_update ${STEAMAPPID} -beta ${STEAM_TEST_BRANCH} validate +quit
-else
-    echo "Using steamcmd default branch"
-    ${STEAMCMDDIR}/steamcmd.sh +force_install_dir ${STEAMAPPDIR} +login anonymous +app_update ${STEAMAPPID} validate +quit
+if [ "${SKIP_INSTALL}" == "false" ]; then
+    echo "APP <ID:${STEAMAPPID}> Install directory will be ${STEAMAPPDIR}"
+    if [ "${STEAM_TEST_BRANCH}" != "" ]; then
+        echo "Using steamcmd beta branch ${STEAM_TEST_BRANCH}"
+        su - steam -c "${STEAMCMDDIR}/steamcmd.sh +force_install_dir ${STEAMAPPDIR} +login anonymous +app_update ${STEAMAPPID} -beta ${STEAM_TEST_BRANCH} validate +quit"
+    else
+        echo "Using steamcmd default branch"
+        su - steam -c "${STEAMCMDDIR}/steamcmd.sh +force_install_dir ${STEAMAPPDIR} +login anonymous +app_update ${STEAMAPPID} validate +quit"
+    fi
 fi
 
 # Update config (will reset on each steamcmd update)
 sed -i "s=Xmx8g=Xmx${CONFIG_XMX}g=g" ${CONFIG_PATH}
 sed -i "s=UseZGC=UseG1GC=g" ${CONFIG_PATH}
 
-${STEAMAPPDIR}/start-server.sh -servername ${SERVER_NAME} -adminpassword ${ADMIN_PASSWORD} ${ADDITIONAL_ARGS}
+export HOME=${HOMEDIR}
+su - steam -c "${STEAMAPPDIR}/start-server.sh -servername ${SERVER_NAME} -adminpassword ${ADMIN_PASSWORD} ${ADDITIONAL_ARGS}"
